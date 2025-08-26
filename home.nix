ba233d27c0e2a7822b81f3115a2728606b647495
BraeTroutman <btroutma@redhat.com>
2025-08-25 20:19:42 -0400

    correct comments

--- home.nix
+++ home.nix
@@ -1,22 +1,16 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+  #
+  # HOME MANAGER CONFIG
+  # 
   home.username = "btroutma";
   home.homeDirectory = "/home/btroutma";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "25.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
+  #
+  # INSTALLED PACKAGES
+  # 
   home.packages = [
     # Red Hat specific dependencies
     pkgs.rosa
@@ -39,8 +33,9 @@
     pkgs.delve
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
+  #
+  # DIRECT DOTFILES
+  # 
   home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
@@ -54,22 +49,9 @@
     # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
   #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/btroutma/etc/profile.d/hm-session-vars.sh
-  #
+  # ENVIRONMENT VARIABLES
+  # 
   home.sessionVariables = {
     EDITOR = "hx";
     SHELL = "zsh";
@@ -79,7 +61,9 @@
     ANTHROPIC_VERTEX_PROJECT_ID = "itpc-gcp-hcm-pe-eng-claude";
   };
 
-  # Let Home Manager install and manage itself.
+  #
+  # PROGRAMS
+  # 
   programs.home-manager.enable = true;
 
   programs.helix = {
